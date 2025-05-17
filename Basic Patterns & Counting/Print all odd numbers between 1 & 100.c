cf7d6cc79d4655e815f82a2867c82759b551120a
unknown <rayane>
2025-05-17 17:24:39 +0100

    Added 3 problem solution,P_1 : print all nmbrs from 1 to 100 , P_2 : Print all even numbers between 1 & 100 , P_3 : Print all odd numbers between 1 & 100

---
+++ Basic Patterns & Counting/Print all odd numbers between 1 & 100.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+    for(int i = 1 ; i <= 100 ; i++){//a counter to count from 1 to 100
+        //check if the number is odd if it is print it
+		if(i % 2 != 0)
+			printf("%i ",i);//print the value of the counter
+    }
+}
---
+++ Basic Patterns & Counting/Print numbers from 1 to 100.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    for(int i = 1 ; i <= 100 ; i++){//a counter to count from 1 to 100
+        printf("%i ",i);//print the value of the counter every lap
+    }
+}
